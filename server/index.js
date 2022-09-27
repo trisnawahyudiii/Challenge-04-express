bfa4c307a779436afe89e96800e5d800520176d2
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-09-27 22:21:11 +0800

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const PORT = process.env || 8000;
+const fs = require('fs');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
 
+// cars
+app.get('/', (req, res) => {
+    res.render('index');
+});
+app.get('/cars', (req, res) => {
+    const data = require('../data/cars.json');
+
+    res.render('cariMobil', { data: data });
+});
 
 app.listen(PORT, () => {
     console.log(`App Running at http://localhost:${PORT}`);
